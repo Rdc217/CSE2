b649dc5ac47ec398682291277c4da687a4da0848
rdc217 <rdc217@lehigh.edu>
2014-08-28 00:43:29 +0000

    First homework

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,26 @@
+//Ryan Cooney
+//CSE2 Section 111
+//8-27-14
+//Welcome Class Java Program
+
+//define a class
+public class WelcomeClass{
+    
+//add main method
+public static void main(String[] args)  {
+    
+   // print this statement with a box around it
+   System.out.println("  -----------");
+   System.out.println("  | WELCOME |");
+   System.out.println("  -----------");
+   System.out.println("  ^  ^  ^  ^  ^  ^");
+   System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+   System.out.println("|-R--D--C--2--1--7-|");
+   System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+   System.out.println("  v  v  v  v  v  v");
+   //tweet length statement
+   System.out.println("I am a member of the Cross Country team at Lehigh.");
+   System.out.println("This summer I ran almost 1000 miles!"); 
+   //end   
+   }
+}
